deb16af90015bf129858ec665ecb5611b8577142
Giridharan7193 <giridharan7193gs@gmail.com>
2025-03-10 16:52:29 +0530

    Fourth Commit by theGIRIDHARAN

--- src/app/Components/ChatBot.js
+++ src/app/Components/ChatBot.js
@@ -16,8 +16,8 @@ export default function ChatBot() {
         setInput("");
         setLoading(true);
 
-        try { 
-            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/chat`, { message: input });
+        try {
+            const response = await axios.post(`/api/chat`, { message: input });
 
             setMessages((prevMessages) => [
                 ...prevMessages,
